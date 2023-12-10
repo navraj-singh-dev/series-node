773f5041623d9f778c3bf14af72a4eeabf448f73
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-12-10 22:03:18 +0530

    update 01 hello word folder

--- 01 hello-world/file.js
+++ 01 hello-world/file.js
@@ -1,31 +1,31 @@
 const fs = require("fs");
 const os = require("os");
 
-// // sync
-// fs.writeFileSync( "./test.txt", "write synccc" );
+// sync
+fs.writeFileSync( "./test.txt", "write synccc" );
 
-// // async
-// fs.writeFile( "./test.txt", "readFile", (err) => {
-//     console.log("error")
-// });
+// async
+fs.writeFile( "./test.txt", "readFile", (err) => {
+    console.log("error")
+});
 
-// // sync
-// const result = fs.readFileSync("./contacts.txt", "utf-8");
-// console.log(result)
+// sync
+const result = fs.readFileSync("./contacts.txt", "utf-8");
+console.log(result)
 
-// // async
-// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
-//     if (err) {
-//         console.log("error", err);
-//     } else {
-//         console.log(result) 
-//         // async dont return 
-//     }
-// });
+// async
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+    if (err) {
+        console.log("error", err);
+    } else {
+        console.log(result) 
+        // async dont return 
+    }
+});
 
 
-// fs.appendFileSync( "./hello-world/test.txt", "\nappended text");
-// fs.cpSync("./hello-world/test.txt", "./hello-world/copy.txt");
-// fs.unlinkSync("./hello-world/copy.txt");
+fs.appendFileSync( "./hello-world/test.txt", "\nappended text");
+fs.cpSync("./hello-world/test.txt", "./hello-world/copy.txt");
+fs.unlinkSync("./hello-world/copy.txt");
 
 console.log( os.cpus().length );
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # series-node
- 
+
+Performing "learning in public", I will be doing -Node[Backend in MERN]- from scratch and use github as my journal for it.
